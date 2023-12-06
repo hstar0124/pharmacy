71513ceb4549426bb57d7006cbd4baf12a0a0564
hstar0124 <dunjun10@naver.com>
2023-12-06 15:15:41 +0900

    add jpa-auditing-code

--- src/main/java/com/hoya/project/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/hoya/project/pharmacy/entity/Pharmacy.java
@@ -1,5 +1,6 @@
 package com.hoya.project.pharmacy.entity;
 
+import com.hoya.project.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
