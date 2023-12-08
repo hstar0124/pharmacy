989bd594967e237339d270673b7baf5865344e4b
hstar0124 <dunjun10@naver.com>
2023-12-08 10:57:32 +0900

    add pharmacy search logic, test code

---
+++ src/main/java/com/hoya/project/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,41 @@
+package com.hoya.project.pharmacy.service;
+
+import com.hoya.project.pharmacy.dto.PharmacyDto;
+import com.hoya.project.pharmacy.entity.Pharmacy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+
+        // redis
+
+        //db
+        return pharmacyRepositoryService.findAll()
+                .stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+
+    }
+}
